84ffcfe141c6e4eb8c15116186c36ca56816b186
PravdiilShilinskas <mr.pravdiil@mail.ru>
2017-11-08 11:45:32 +0300

    Update IzAvB.cpp

--- Graff/IzAvB.cpp
+++ Graff/IzAvB.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int shirina(int* evo, int* evd, int e, int k, int v, int n){
+int poisk(int* evo, int* evd, int e, int k, int v, int n){
 if(v>1){
    for(int i=0;i<e;i++){
-      if(evo[i]==k){
-         if((evd[i]==n)||(shirina(evo, evd, e, evd[i], v, n)==1)){
+      if((evo[i]==k)||(evd[i]==k)){
+         if((evd[i]==n)||(poisk(evo, evd, e, evd[i], v, n)==1)||(evo[i]==n)||(poisk(evo, evd, e, evo[i], v, n)==1)){
             cout << i << "   ";
             return 1;
          }
@@ -24,7 +24,7 @@ void Graff(int e, int v, int m, int n){
    for(int i=0;i<e;i++){
       cin >> evo[i] >> evd[i];
    };
-   shirina(evo, evd, e, m, v, n);
+   poisk(evo, evd, e, m, v, n);
 }
 
 int main(){
