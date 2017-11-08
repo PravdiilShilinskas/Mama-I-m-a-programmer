fd5e6775d92392ab87b45d673ba9b2a2d27a8b8c
PravdiilShilinskas <mr.pravdiil@mail.ru>
2017-11-08 11:46:46 +0300

    Update and rename IzAvB.cpp to Doroga.cpp

--- Graff/Doroga.cpp
+++ Graff/Doroga.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int poisk(int* evo, int* evd, int e, int k, int v, int n){
+int doroga(int* evo, int* evd, int e, int k, int v, int n){
 if(v>1){
    for(int i=0;i<e;i++){
       if((evo[i]==k)||(evd[i]==k)){
-         if((evd[i]==n)||(poisk(evo, evd, e, evd[i], v, n)==1)||(evo[i]==n)||(poisk(evo, evd, e, evo[i], v, n)==1)){
+         if((evd[i]==n)||(doroga(evo, evd, e, evd[i], v, n)==1)||(evo[i]==n)||(doroga(evo, evd, e, evo[i], v, n)==1)){
             cout << i << "   ";
             return 1;
          }
@@ -24,7 +24,7 @@ void Graff(int e, int v, int m, int n){
    for(int i=0;i<e;i++){
       cin >> evo[i] >> evd[i];
    };
-   poisk(evo, evd, e, m, v, n);
+   doroga(evo, evd, e, m, v, n);
 }
 
 int main(){
